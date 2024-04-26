c761f563ba9e84ef9794a89fb186435d0dfe1f98
Flavio <flaviocicero55@gmail.com>
2024-04-26 16:37:33 +0200

    foolproofed + shortened
    it is illegible

--- mastermind.py
+++ mastermind.py
@@ -1,38 +1,18 @@
-import math
-import numpy as np
 import mastermind_lib as m
-
 yes = ["yes", "y", "ye", "yuh", "ya"]
-
-def UIStartup():
-    print("\n Welcome to Mastermind! Here are the rules: \n 1) You have 10 attempts to guess the correct combination \n 2) You may only enter numbers ranging from 1 to 6 \n 3) X signifies the number is incorrect, 0 indicates your number is present but incorrectly placed. # indicates the number is present and correctly placed.")
-
 def inpput():
-    answer = input("Input answer: ").replace(" ","")
-    ans = []
-    if len(answer) > 4:
+    answer, ans = input("Input answer: ").replace(" ",""), []
+    if len(answer) > 4 or answer.isdigit() == False:
         print("Please submit a four digit number ")
         answer = input("Input answer: ").replace(" ", "")
     else: ans += [answer[i] for i in range(4)]
     return m.toInt(ans)
-
 def game():
-    global correctFactor
-    global TF
-    global ans
-    global screen
-    global balls
-    balls = m.gen()
-    screen = []
-    TF = []
-    attempts = 0
-    #Reads better than a for loop
+    global TF, ans, screen, balls, correctFactor
+    screen, TF, attempts, balls = [], [], 0, m.gen()
     while attempts < 11:
         ans = inpput()
-        correctFactor = 0
-        FullC = 0
-        HalfC = 0
-        Inc = 0
+        FullC, HalfC, Inc, correctFactor = 0, 0, 0, 0
         for i in range(0,4):
             if int(ans[i]) in balls:
                 if int(ans[i]) == balls[i]:
@@ -43,28 +23,24 @@ def game():
         TF.append("#"*FullC + "0"*HalfC + "X"*Inc)
         attempts += 1
         if correctFactor == 4:
-            attempts = 11
             win()
+            break
         if attempts == 10: lose()
         print(attempts)
         UI(attempts-1)
-
 def UI(iteration):
     print("\n")
     screen.append(m.toStr(ans) + " | " + m.toStr(TF[iteration])+ "\n")
     print(m.toStr(screen))
-
 def win():
     repeat = input("Congratulations, you have won! Would you like to play again? ")
     if repeat.lower() in yes: main()
     else: exit()
-
 def lose():
     repeat = input("You have lost. Would you like to play again? ")
     if repeat.lower() in yes: main()
     else: exit()
-
 def main():
-    UIStartup()
+    print("\n Welcome to Mastermind! Here are the rules: \n 1) You have 10 attempts to guess the correct combination \n 2) You may only enter numbers ranging from 1 to 6 \n 3) X signifies the number is incorrect, 0 indicates your number is present but incorrectly placed. # indicates the number is present and correctly placed.")
     game()
 main()
--- mastermind_lib.py
+++ mastermind_lib.py
@@ -1,6 +1,4 @@
 import numpy as np
-import math as m
-
 #generates non conscecutive random 4 digit number
 def gen():
     balls = []
@@ -12,13 +10,11 @@ def gen():
         numbers.pop(int(index_random))
         n += -1
     return balls
-
 #converts values of list to integers
 def toInt(list):
     for i in range(len(list)):
         list[i] = int(list[i])
     return list
-
 #converts list to a single string
 def toStr(list):
     string = ""
